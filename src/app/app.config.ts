83b9c0a0340bb3e501e8a9d7cefbf8d7e5e439b3
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-09-14 19:46:27 +0200

    add api url token to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { environment } from './environments/environment';
+import { API_URL } from './core/injection-tokens/api-tokens';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    { provide: API_URL, useValue: environment.apiUrl }
   ]
 };
