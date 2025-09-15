9bd318b8b9d16603db74510357a8727703b26664
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-09-15 21:23:09 +0200

    add not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,5 +10,10 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./features/components/login/login.component')
       .then(c => c.LoginComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./shared/components/not-found/not-found.component')
+      .then(c => c.NotFoundComponent)
   }
 ];
