bb06217c285a08e150f1371c7415c0463c7f90fc
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-09-15 18:42:23 +0200

    add login page

---
+++ src/app/features/auth/login/login.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AuthService } from '../../../core/services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    CommonModule,
+    MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
+  public hidePassword: boolean = true;
+
+  private returnUrl: string = '/';
+  private readonly afterLoginUrl: string = '/';   // NOTE: Change the route depending on your need
+
+  constructor(private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(10), Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d).{10,}$/)]]
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.authService.isAuthenticated()) {
+      this.router.navigate([this.afterLoginUrl]);
+    }
+    else {
+      this.returnUrl = this.activatedRoute.snapshot.queryParamMap.get('returnUrl') ?? this.afterLoginUrl;
+    }
+  }
+
+  onTogglePassword(): void {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onLogIn(): void {
+    // NOTE: Log in to the system and route the user to the 'returnUrl'
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+}
