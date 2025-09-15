0625743bc12d6e57d091329f4e7277a755e202f1
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-09-15 21:19:11 +0200

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/components/home/home.component')
+      .then(c => c.HomeComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/components/login/login.component')
+      .then(c => c.LoginComponent)
+  }
+];
